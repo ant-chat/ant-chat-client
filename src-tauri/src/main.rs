ad28cabe096094aa9dcd5d28aa200a1143a38a04
WonbaeLee <gloomygentleman@gmail.com>
2023-04-29 19:14:35 +0900

    init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,16 @@
+// release mode 에서 Windows의 추가 콘솔창을 방지 합니다.
+// ## 절대 지우지 마세요!! ##
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
